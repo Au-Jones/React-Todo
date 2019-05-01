9ab200e65158ecb203770579d138cfc0b46918de
Your Name <aujones95@gmail.com>
2019-05-01 16:32:48 -0400

    finished the app.js

--- src/App.js
+++ src/App.js
@@ -14,3 +14,79 @@ class App extends React.Component {
 }
 
 export default App;
+// task
+// ID
+// completed
+
+const tasks = [
+ {
+  Task: "Bathe Dogs",
+  ID: 1,
+  Completed: false
+ },
+ {
+  Task: "Take out garbage",
+  ID: 2,
+  Completed: false
+ },
+ {
+Task: "Wash Dishes",
+ID: 3,
+Completed: false
+ },
+]
+
+class ToDo extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      TodoList: tasks,
+      Task:"",
+      ID:"",
+      Completed:""
+    };
+  }
+
+
+  changeHandler = (e) => {
+    this.setState({
+      [e.target.Task]: e.target.value
+    });
+  };
+
+  addTask = (e) => {
+    e.preventDefault();
+    this.setState({
+      TodoList:[
+        ...this.state.TodoList,
+        {
+          Task: this.state.Task,
+          ID: this.state.ID,
+          Completed: this.state.Completed
+        }
+      
+      ],
+      Task: "",
+      ID: "",
+      Completed: ""
+    });
+  };
+
+  render() {
+    return(
+      <div>
+        <h1>ToDo List</h1>
+        <TaskList TodoList={this.state.TodoList} />
+        <TodoForm
+        addTask={this.addTask}
+        changeHandler={this.ChangeHandler}
+        Task={this.state.Task}
+        ID={this.state.ID}
+        Completed={this.state.Completed}
+        />
+      </div>
+    );
+  }
+}
+
+
